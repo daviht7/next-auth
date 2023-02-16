9afb85d58d272fd80573b76b83c24edfcf718fb7
Davi Holanda <daviht7@gmail.com>
2023-02-16 19:29:01 -0300

    realizando refresh do token

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,13 @@
+import { api } from "@/services/api";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  api.get("/me").then((response) => {
+    console.log("response", response.data);
+  });
 
   return <h1>DASHBOARD : {user?.email}</h1>;
 }
